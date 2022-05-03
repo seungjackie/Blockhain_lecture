e94545cbe93ffd2b024e60508581bfac627dac15
seungjackie <qkrtmdwo28@gmail.com>
2022-05-03 12:39:49 +0900

    asdf

--- blockchain/day3/block.js
+++ blockchain/day3/block.js
@@ -22,7 +22,7 @@ const calculateHash = (index, data, timestamp, previousHash, difficulty, nonce) 
 // 16진수 1자리 -> 2진수 4자리 256개의 0과 1로 표현 
 
 const createGenesisBlock = () => {
-    const genesisBlock = new Block(0, 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks', new Date().getTime() / 1000, 0, 0, 0, 0);
+    const genesisBlock = new Block(0, 'The Times 03/Jan/2009 Chancellor on brink of second bailout for banks', /* new Date().getTime() / 1000 */ 0, 0, 0, 0, 0);
 
     genesisBlock.hash = calculateHash(genesisBlock.index, genesisBlock.data, genesisBlock.timestamp, genesisBlock.previousHash, genesisBlock.difficulty, genesisBlock.nonce);
     
