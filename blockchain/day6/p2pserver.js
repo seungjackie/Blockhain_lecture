83c694e7032b8721f10269edab769c58523c93cf
seungjackie <qkrtmdwo28@gmail.com>
2022-05-09 11:37:03 +0900

    asdf

---
+++ blockchain/day6/p2pserver.js
@@ -0,0 +1,172 @@
+// pear two pear  > express 가 아닌 웹소켓을 쓴다.
+// 블록체인은 중앙서버가 있는것이 아닌 개인과 개인을 통해서 데이터가 이루어짐
+// 다른 노드와 통신을 위한 서버 
+
+import WebSocket from 'ws';
+import { WebSocketServer } from 'ws';
+import { getBlocks, getLatestBlock , createBlock,addBlock , isValidNewBlock , replaceBlockchain} from './block.js';
+
+const MessageType = {
+    // RESPONCE_MESSAGE : 0,
+    // SENT_MESSAGE : 1,
+
+    // 최신 블록 요청
+    QUERY_LATEST : 0,
+    // 모든 블록 요청
+    QUERY_ALL : 1,
+    // 블록 전달
+    RESPONSE_BLOCKCHAIN : 2,
+    QUERY_TRANSACTION_POOL : 3,
+    RESPONSE_TRANSACTION_POOL : 4
+
+}
+
+const sockets = [] ; // 상수로 만들었다. sockets의 메모리 주소 push 들어가는 데이터 값과는 별개이다.
+// sockets = 1; // sockets의 값을 바꾸는 것이기 떄문에 안된다.
+
+const getPeers = () => {
+    return sockets;
+}
+
+const initP2PServer = (p2pPort) => {
+    const server = new WebSocketServer({port:p2pPort});
+    server.on('connection', (ws) => {  // 웹소켓으로 만든 서버에 호출을 한다.
+        initConnection(ws); // 내부 함수가 아닌 만들어 줘야하는 함수
+        initMessageHandler(ws);
+    })
+    console.log('listening P2PServer Port : ', p2pPort);
+}
+
+//다른 사람의 Peer 추가했을때 initConnection
+const initConnection = (ws) => {
+    sockets.push(ws);
+    console.log('성공');
+
+    // 가지고 있는거 다 줘봐
+    write(ws, responseAllMessage())
+    // initMessageHandler(ws);
+    // ws.onmessage((e)=> {console.log(e.data)});
+}
+
+const connectionToPeer = (newPeer) => { // 새로운 peer주어지면 접속할수 있는 환경을 만든다.
+    const ws = new WebSocket(newPeer);
+    // ws.on('open', () => { initConnection(ws); return true})
+    // ws.on('error', () => { console.log('Fail to Connection peer : ', ws.remoteAddress); return false})
+    ws.on('open', () => { initConnection(ws); console.log('Connect peer : ', newPeer);})
+    ws.on('error', () => { console.log('Fail to Connection peer : ', newPeer); })
+}
+
+const initMessageHandler = (ws) => {
+    ws.on('message', (data) => {
+        const message = JSON.parse(data);
+        // () message.type = 0
+
+        switch(message.type) 
+        {
+            case MessageType.QUERY_LATEST:  // 응답을 받으면 다시 보내줘야한다.
+                break;
+            case MessageType.QUERY_ALL: // 블록을 요청
+                // 요청 하면보내주면 된다.
+                write(ws, responseAllMessage());        // 응답
+                break;
+            case MessageType.RESPONSE_BLOCKCHAIN:       // 누군가 내가 요청한 블록을 보내주었다. (RESPONSE_BLOCK)
+                // 넘어오는 부분
+                console.log(ws._socket.remoteAddress, ':' , message.data);
+                // replaceBlockchain(message.data);
+                handleBlockchainResponse(message.data)
+                break;
+            case MessageType.QUERY_TRANSACTION_POOL:
+                break;
+            case MessageType.RESPONSE_TRANSACTION_POOL:
+                break;
+        }
+    })
+}
+
+
+
+
+
+const handleBlockchainResponse = (receiveBlockchain) => {
+    const newBlocks = JSON.parse(receiveBlockchain);
+    // 받아온 블록의 마지막 인덱스가  내 마지막 블록의 인덱스 보다 크다.
+    const latestNewBlock = newBlocks[newBlocks.length -1];
+    console.log('받아온 마지막 블록 :' , latestNewBlock);
+    const latestMyBlock = getLatestBlock();
+    console.loge('마지막 블록' , latestMyBlock)
+
+    if ( latestNewBlock.index > latestMyBlock.index){
+        // 받아온 마지막 블록의 피리비어스 해쉬 와 내 마지막 블록의 해쉬를 확인한다./
+        if(latestNewBlock.previousHash == latestMyBlock.previousHash) {
+            if(addBlock(latestNewBlock, latestMyBlock)) {
+                // 제한된 플러딩을 사용한다.
+                broadcasting(responseLatestMessage());
+            }
+        }
+        // 받아온 크기가 전체 크기가 1인 경우 -> 재요청
+        else if (newBlocks.length ===1 )
+        {
+            broadcasting(queryAllMessage());
+        }
+
+        // 그외
+        // 받은 블록체인보다 현재 블록체인이 더 길다.(안 바꿈)
+        // 같으면 . (바꾸거나 안 바꿈)
+        // 받은 블록체인이 현재 블록체인보다 길면 바꾼다.( 바꿈)
+        else {
+            replaceBlockchain(newBlocks);
+        }
+    }
+}
+
+const queryLatestMessage = () => { // 다른 노드에게 다른 메세지를 발생시키는 함수
+    return ({   
+            "type" : MessageType.QUERY_LATEST,
+            "data" : null   })
+}
+
+// 없다.
+const queryAllMessage = () => { // 다른 노드에 전체블록을 메세지를 만드는 함수
+    return ({   
+            "type" : MessageType.QUERY_ALL,
+            "data" : null   })
+}
+
+// 배열로 넘겨주기, 형태가 다르기 때문
+const responseLatestMessage = () => { // 요청을 받았을때 나의 마지막 블럭을 요청한 쪽에 전달을 해주는 함수
+    return ({   
+        "type" : MessageType.RESPONSE_BLOCKCHAIN,
+        "data" : JSON.stringify([getLatestBlock()])   })  /* (내가 가지고 있는 체인의 마지막 블록) */
+}
+
+const responseAllMessage = () => { 
+    return ({   
+        "type" : MessageType.RESPONSE_BLOCKCHAIN,
+        "data" : JSON.stringify(getBlocks())   }) /* (내가 가지고 있는 전체블록) */
+}
+
+const write = (ws, message) => { //JSON TYPE을 하나의 문자열을 바꿔서 보낸다.
+    // console.log('write()', ws_socket.remoteAddress, ':', message.message);
+    ws.send(JSON.stringify(message)); 
+}
+
+// 우리가 등록한 소켓들이 하나씩 들어있는데 0 번 index부터 끝 index 까지 반복이 되는데 우리가 등록댐 것이 호출이 된다.
+const broadcasting = (message) => {
+    sockets.forEach((socket) => {
+        write(socket, message)
+    });
+}
+
+// 채굴 블록 , 외부에서 가져다 쓰겠다
+const mineBlock = (blockData) => {
+    const newBlock = createBlock(blockData);
+    console.log(newBlock)
+    if(addBlock(newBlock, getLatestBlock())){
+        broadcasting(responseLatestMessage());
+    }
+}
+
+
+
+
+export { initP2PServer, connectionToPeer, getPeers, broadcasting , mineBlock};
---
+++ blockchain/day6/transaction.js
@@ -0,0 +1,228 @@
+import CryptoJS from 'crypto-js';
+import _ from 'lodash';
+import { getPublicKeyFromWallet,getPrivateKeyFromWallet} from './wallet.js'
+
+const COINBASE_AMOUNT = 50;
+
+let transactionPool = [];
+
+const getTransactionPool = () => {
+    return _.cloneDeep(transactionPool);    // 깊은 복사
+}
+
+let unspnetTxOuts = [];                     // UnspentTxOut[]
+
+
+class UnspentTxOut {
+    constructor(txOutId, txOutIndex, address,amount){
+        this.txOutId = txOutId;
+        this.txOutIndex = txOutIndex;
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+let trans = [...transactionPool]            // 깊은 복사
+
+// 코인을 어디로 얼만큼 보냈는가
+class TxOut{
+    constructor(address, amount) {
+        this.address = address;             // string
+        this.amount = amount;               // number
+    }
+}
+
+// 보내진 코인을 실제로 소유했다에 대한 증거
+class TxIn{
+    constructor(txOutId, txOutIndex, sign) {
+        this.txOutId = txOutId;             // string
+        this.txOutIndex = txOutIndex;       // number
+        this.sign = sign;                   // string
+    } 
+}
+
+
+class Transaction {
+    constructor(id, txIns, txOuts) {
+        this.id = id;                       // string
+        this.txIns = txIns;                 // TxIn[]
+        this.txOuts = txOuts;               // TxOut[]
+    }
+}
+
+// transaction ID
+const getTransactionID = (transaction) => {
+    // txIns에 있는 내용들을 하나의 문자열로 만든다.
+    const txInsContent = transaction.txIns
+        // 배열 string
+        .map((txIn) => txIn.txOutId + txIn.OutIndex)
+        .reduce((a, b) =>  a + b, '' );
+
+    // txOuts에 있는 내용들을 하나의 문자열로 만든다.
+    const txOutsContent = transaction.txIns
+        .map((txOut) => txOut.address + txOut.amount)
+        .reduce((a, b) => { a + b, ''} )
+    
+
+
+    // 위의 두내용을 다 합해서 hash 처리한다.
+    CryptoJS.SHA256(txInsContent + txOutsContent ).toString();
+}
+
+
+// transaction signature
+const signTxIn = (transaction, txIndex, privateKey) => {
+    // 보내고 받아야하는 부분.
+    // const txIn = transaction.txIns[txIndex];        // txIns 에서 가져오기
+
+    // TODO :sign 코드 검증
+    const signature = toHexString(privateKey, transaction.id).toDER();       // id 선언
+    return signature;
+}
+
+// coinbase Transaction
+const getCoinbaseTransaction = (address, blockIndex) => {
+    const tr = new Transaction();
+    const txIn = new TxIn();
+    txIn.sign = '';
+    txIn.txOutId = '';
+    txIn.txOutIndex = blockIndex;
+
+    // 표시
+    tr.txIns = [txIn];
+
+    const txOut = new TxOut();
+    txOut.address = address;
+    txOut.amount = COINBASE_AMOUNT;
+
+    tr.txIns = [txIn];
+    tr.txOuts = [txOut];
+    tr.id = getTransactionID(tr);
+
+    return tr;
+}
+
+const sendTransaction = (address, amount) => {
+    // 1. 1. 트랜젝션 생성
+    const tx = createTransaction();
+
+    // 2. 트랜젝션 폴더 추가
+    transactionPool.push(tx);
+    
+    // 3. 주변 노드에 전파
+    
+
+    return tx;
+}
+
+const createTransaction = (amount , address) => {
+    // 1. 아직 처리 되진 않았지만 트랜잭션 풀에 올라가 있는 내용을 확인
+    const myAddress = getPublicKeyFromWallet();
+    const myUnspentTxOuts = unspnetTxOuts.filter((uTxO) => uTxO.address === myAddress);                 // 하나의 요소
+    // Todo : ArrowFunction 형태 확인
+    // unspnetTxOuts.filter((uTxO) => uTxO.address === myAddress);                 // 하나의 요소
+
+    const filteredUnspentTxOuts=filterTxPoolTxs(myUnspentTxOuts);
+
+    // 2. 거래애 사용되지 않은 Txouts을 구성, 트랜젝션에 필요한 코인을 확인
+    // 넘기는 금액은 다시 나한테 전달
+    // amount 는 외부에서 받아온다
+    // return 값이 두개
+    const {includeTxOuts, leftoverAmount } =findTxOutsForAmount(amount, filteredUnspentTxOuts);
+
+    // 3. 서명 전의 TxIns로 구성
+    const unsignedTxIns = includeTxOuts.map(createUnsignedTxIn);
+
+    includeTxOuts
+    createUnsignedTxIn
+
+    // 4. 트랜젝션 구성
+    const tx = new Transaction();
+    tx.txIns = unsignedTxIns;                                             // 서명 전
+    tx.txOuts = createTxOuts(address, amount, leftoverAmount);            // 받는사람 주소
+    tx.id = getTransactionID(tx);
+
+    // 서명 바뀐값을 새로운 요소로 만든다.
+    tx.txIns = tx.txIns.map( (txIn) => {
+        txIn.sign = signTxIn(tx, tx.txIns.txOutIndex, getPrivateKeyFromWallet());
+        return txIn;
+    });
+
+    return tx;
+}
+
+const filterTxPoolTxs = (myUnspentTxOuts) => {
+    // 트랜젝션 풀에서 트랜잭션 인풋 내용만 추출
+    const txIns = _(transactionPool)
+            .map((tx)=> tx.txIns)           //매개변수 가져온다, 새로운 배열을 구성한다
+            .flatten()
+            .value();
+
+    console.log('트랜젝션 : ' , transactionPool );
+    console.log("트랜젝션 풀안의 inputs : " , txIns );
+
+    const removeable = [];
+
+    // 함수형 프로그래밍
+    // 조건에 맞는 요소들을 찾아낸다
+    for (const unspentTxOut of myUnspentTxOuts) {
+        // 저장
+        const findTxIn = _.find(txIns, (txIn) => {
+            // 하나하나 비교.
+            return txIn.txOutIndex === unspentTxOut.txOutIndex && 
+                txIn.txOutId === unspentTxOut.txOutId 
+        })
+        // 찾았는데 빈값이다.
+        if(findTxIn == undefined) {
+
+        }
+        // 뭔가를 찾아 냈다.
+        else {
+            // 트랜젝션 풀에 올라가 있는것들과 비교
+            removeable.push(unspentTxOut);
+        }
+    }
+
+    // 제거
+    _.without(myUnspentTxOuts, ...removeable)
+}
+
+const findTxOutsForAmount = (amount, filteredUnspentTxOuts) => {
+    // 누적금액
+    let currentAmount = 0;
+    const includeTxOuts = [];
+
+    for (const unspentTxOut of filteredUnspentTxOuts) {
+        // 내 금액 있는거에서 가져간다.
+        includeTxOuts.push(unspentTxOut);
+        currentAmount = currentAmount + unspnetTxOuts.amount; 
+        if(currentAmount >= amount) {           // 보낼수 있는 경우
+            // 누적된 금액
+            const leftoverAmount = currentAmount - amount;
+            return {includeTxOuts, leftoverAmount};
+        }
+    }
+
+    // 에러
+    throw Error('보내려는 금액보다 보유 금액이 적다 !!');
+}
+
+const createUnsignedTxIn = (unspentTxOut) => {
+    const txIn = new TxIn();
+    txIn.txOutId = unspentTxOut.txOutId;
+    txIn.txOutIndex = unspentTxOut.txOutIndex;
+
+    return txIn;
+}
+
+const createTxOuts =(address, amount, leftoverAmount) => {
+    const txOut = new TxOut(address, amount);
+    if (leftoverAmount > 0){
+        // 내 주소
+        const leftoverTxOut = new TxOut(getPublicKeyFromWallet(), leftoverAmount)
+        return [leftoverTxOut, txOut];
+    }
+    else {
+        return [txOut];
+    }
+}
