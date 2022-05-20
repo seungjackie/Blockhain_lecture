54f865c5dc5a4f83c7a852374107b59e336b117e
seungjackie <qkrtmdwo28@gmail.com>
2022-05-20 16:42:06 +0900

    aasdf

---
+++ React/netflix/src/page/MovieDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieDetail = () => {
+  return (
+    <div>MovieDtail</div>
+  )
+}
+
+export default MovieDetail
---
+++ blockchain/220519/ToHexStringTest.js
@@ -0,0 +1,7 @@
+const toHexString = (byteArray) => {
+    return Array.from(byteArray, (byte) => {
+        return ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    }).join('');
+}
+
+console.log(toHexString([1, 30]));
